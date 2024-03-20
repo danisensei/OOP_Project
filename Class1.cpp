1a34938725e7179b75790de03d632796f04a1f64
Malik Daniyal Ahmad <146337576+danisensei@users.noreply.github.com>
2024-03-20 22:41:27 +0500

    Create Class1.cpp

---
+++ Class1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+class Animal {
+protected:
+    std::string name;
+    std::string sound;
+
+public:
+    Animal(std::string _name, std::string _sound) : name(_name), sound(_sound) {}
+
+    virtual void makeSound() const {
+        std::cout << name << " says " << sound << std::endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    Dog(std::string _name) : Animal(_name, "Woof") {}
+};
+
+class Cat : public Animal {
+public:
+    Cat(std::string _name) : Animal(_name, "Meow") {}
+};
+
+class Cow : public Animal {
+public:
+    Cow(std::string _name) : Animal(_name, "Moo") {}
+};
+
+int main() {
+    Dog dog("Buddy");
+    Cat cat("Whiskers");
+    Cow cow("Bessie");
+
+    dog.makeSound();
+    cat.makeSound();
+    cow.makeSound();
+
+    return 0;
+}
